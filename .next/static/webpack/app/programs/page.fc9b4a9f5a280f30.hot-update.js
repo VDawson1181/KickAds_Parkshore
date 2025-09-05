a0eb77dbc842afe6a0aceafc342bfece49770f47
Val Dawson <VDawson647@gmail.com>
2025-09-05 01:29:29 -0400

    light blue color added

---
+++ .next/static/webpack/app/programs/page.fc9b4a9f5a280f30.hot-update.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/programs/page",{
+
+/***/ "(app-pages-browser)/./app/components/ContentImgLeft/contentImgLeft.css":
+/*!**********************************************************!*\
+  !*** ./app/components/ContentImgLeft/contentImgLeft.css ***!
+  \**********************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"7e05335c80e6\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9jb21wb25lbnRzL0NvbnRlbnRJbWdMZWZ0L2NvbnRlbnRJbWdMZWZ0LmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIi9Vc2Vycy92ZGF3c29uNjQ3L0Rlc2t0b3AvS2lja0Fkcy9QU19DbG9uZS9LaWNrQWRzX1BhcmtzaG9yZS9hcHAvY29tcG9uZW50cy9Db250ZW50SW1nTGVmdC9jb250ZW50SW1nTGVmdC5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCI3ZTA1MzM1YzgwZTZcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/components/ContentImgLeft/contentImgLeft.css\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/components/ContentImgRight/contentImgRight.css":
+/*!************************************************************!*\
+  !*** ./app/components/ContentImgRight/contentImgRight.css ***!
+  \************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"24ae89552e29\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9jb21wb25lbnRzL0NvbnRlbnRJbWdSaWdodC9jb250ZW50SW1nUmlnaHQuY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksSUFBVSxJQUFJLGlCQUFpQiIsInNvdXJjZXMiOlsiL1VzZXJzL3ZkYXdzb242NDcvRGVza3RvcC9LaWNrQWRzL1BTX0Nsb25lL0tpY2tBZHNfUGFya3Nob3JlL2FwcC9jb21wb25lbnRzL0NvbnRlbnRJbWdSaWdodC9jb250ZW50SW1nUmlnaHQuY3NzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiMjRhZTg5NTUyZTI5XCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/components/ContentImgRight/contentImgRight.css\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/components/MainCopyBlock/mainCopyBlock.css":
+/*!********************************************************!*\
+  !*** ./app/components/MainCopyBlock/mainCopyBlock.css ***!
+  \********************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"81976e750c38\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9jb21wb25lbnRzL01haW5Db3B5QmxvY2svbWFpbkNvcHlCbG9jay5jc3MiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLGNBQWM7QUFDN0IsSUFBSSxJQUFVLElBQUksaUJBQWlCIiwic291cmNlcyI6WyIvVXNlcnMvdmRhd3NvbjY0Ny9EZXNrdG9wL0tpY2tBZHMvUFNfQ2xvbmUvS2lja0Fkc19QYXJrc2hvcmUvYXBwL2NvbXBvbmVudHMvTWFpbkNvcHlCbG9jay9tYWluQ29weUJsb2NrLmNzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcIjgxOTc2ZTc1MGMzOFwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/components/MainCopyBlock/mainCopyBlock.css\n"));
+
+/***/ })
+
+});
